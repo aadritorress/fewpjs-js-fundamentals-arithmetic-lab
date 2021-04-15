e504657f19b3f412845b8ecd0f5027eeccaaa3d1
Adriana <adriana.amaral@msn.com>
2021-04-15 14:02:37 -0500

    commit tree 7b2e07375d002e29995007367c6e7b40356c9d65

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+
+// let multipy = 2 * 31;
+
+// let random = (Math.random() * 10) + 1
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 9, 15, 20);
